cb1825149a357769b1482c4ef5f9516ddd4c0c0d
Working Tech <k2785233247@gmail.com>
2024-11-12 14:21:20 +0800

    照着教程写出了基本的界面

--- app/src/main/java/com/example/friendlist/MainActivity.java
+++ app/src/main/java/com/example/friendlist/MainActivity.java
@@ -7,18 +7,46 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+
+import com.example.friendlist.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
-            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
-            return insets;
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+        // Set the default fragment_page for the first time entry
+        replaceFragment(new MessageFragment());
+
+        // Onclick listener for bottom navigation items
+        binding.bottomNavigationView.setOnItemReselectedListener(item -> {
+
+            // Judge which item is clicked
+            switch (item.getItemId()) {
+                case R.id.messagesItem: // messages Page
+                    replaceFragment(new MessageFragment());
+                    break;
+                case R.id.contactsItem: // contacts Page
+                    replaceFragment(new ContactFragment());
+                    break;
+                case R.id.settingsItem: // settings Page
+                    replaceFragment(new SettingFragment());
+                    break;
+            }
+
         });
+    }
+
+    // swap the fragment_page for displaying different pages (creating new replacing old)
+    private void replaceFragment(Fragment fragment) {
+        FragmentTransaction transaction = getSupportFragmentManager().beginTransaction();
+        transaction.replace(R.id.frame_layout, fragment);
+        transaction.commit();
     }
 }
