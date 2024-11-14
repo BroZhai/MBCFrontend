3cc423a9801602edbe4c72b54b15b12a1aaf8a6c
Working Tech <k2785233247@gmail.com>
2024-11-14 14:23:42 +0800

    正在设置'添加好友'的'好友申请列表',TODO: 1.写出每个'请求item'的layout样式 2. 确定layout样式内部的'按钮监听'操作 3. 尝试实现'动态页面'的展示逻辑 4. 和UG讨论API方法合并

--- app/src/main/java/com/example/friendlist/AddContactFragment.java
+++ app/src/main/java/com/example/friendlist/AddContactFragment.java
@@ -10,8 +10,10 @@ import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.FrontendApi.FrontendAPIProvider;
@@ -20,6 +22,7 @@ import org.json.JSONException;
 
 import java.net.URI;
 import java.net.URISyntaxException;
+import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -29,6 +32,7 @@ import java.net.URISyntaxException;
 public class AddContactFragment extends Fragment {
 
     FrontendAPIProvider websocket;
+    ArrayList<UserRequest> requestList = new ArrayList<>();
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -38,6 +42,53 @@ public class AddContactFragment extends Fragment {
     // TODO: Rename and change types of parameters
     private String mParam1;
     private String mParam2;
+
+    // 准备提供
+    class MyAdapter extends BaseAdapter {
+        //对于每个Adapter，我们都要去重写以下四个方法 (必须重写！)
+        @Override
+        public int getCount() { // 获取数据的'个数'
+            return requestList.size();
+        }
+
+        @Override
+        public Object getItem(int i) { //获取具体某个'元素'，上面会传入'下标'进来给你定位
+            return requestList.get(i);
+        }
+
+        @Override
+        public long getItemId(int i) {//获取每一个item的唯一标识符"id"(偷懒可直接用下标)
+            return i;
+        }
+
+        @Override
+        //重点: 获取每一个item的'展示样式' (单独开个layout文件设置每个Item的样式)
+        public View getView(int i, View view, ViewGroup viewGroup) {
+            //参数说明: i (item的下标位置)，
+            //view (缓存着'划出视野'的item, 优化listView用)
+            //viewGroup (listView对象本身，用的少)
+
+            //1. 使用View.inflate()加载布局 inflate(Activity对象，布局文件，不知道就填null);
+            //todo: 这里的还没有写出每个'请求item'的layout样式
+//            View item_view = view.inflate(AddContactFragment.this.getActivity(), R.layout., null);
+
+            /* todo: 以下部分要把'请求item'的layout样式写出来之后，才能确认(要修改哪些地方，以及对应'操作按钮'的监听)
+
+            //2. 从取得的View对象中，获取里面 要进行'动态设置'的'小组件' (注意要指定为上面的inflate()的view对象来找[不然默认就会去找'全局的View']XD)
+            TextView capital = (TextView) item_view.findViewById(R.id.capLetter);
+            TextView nameString = (TextView) item_view.findViewById(R.id.nameStr);
+            //Tips: priceLable不用动(设置)，它就是放在那里展示的
+
+            //3. 根据"原数据"设置各个控件的'展示数据'
+            capital.setText(requestList.get(i).getName().substring(0,1));
+            nameString.setText(requestList);
+
+             */
+
+            //4. 返回该View对象 (这样以后，这个Adapter就算建立好了，接下来将这个"格式"应用到listView控件中去)
+            return new View(AddContactFragment.this.getContext());
+        }
+    }
 
     public AddContactFragment() {
         // Required empty public constructor
@@ -88,7 +139,7 @@ public class AddContactFragment extends Fragment {
         EditText input_email = view.findViewById(R.id.emailReq);
         Button sendBtn = view.findViewById(R.id.sendReq);
 
-        initWebSocket();
+        initWebSocket(); // 创建该界面时，初始化WebSocket并尝试建立连接
 
         sendBtn.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -133,6 +184,16 @@ public class AddContactFragment extends Fragment {
         super.onDestroyView();
         if (websocket != null) {
             websocket.close();
+        }
+    }
+
+
+
+    public void clearPendingRequests(){
+        for(int i=0; i<requestList.size(); i++){
+            if(requestList.get(i).isPending()==false){
+                requestList.remove(i);
+            }
         }
     }
 }
