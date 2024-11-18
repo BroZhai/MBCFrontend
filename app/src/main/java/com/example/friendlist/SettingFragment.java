3d48a08f23fd7131cd12580d1c88b7a94c87f264
Working Tech <k2785233247@gmail.com>
2024-11-18 09:49:49 +0800

    在本地实现了'修改功能'，还未与服务器通信,准备一下...

--- app/src/main/java/com/example/friendlist/SettingFragment.java
+++ app/src/main/java/com/example/friendlist/SettingFragment.java
@@ -11,6 +11,7 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.Toast;
 
 /**
@@ -19,6 +20,9 @@ import android.widget.Toast;
  * create an instance of this fragment.
  */
 public class SettingFragment extends Fragment {
+
+    EditText username;
+    EditText password;
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -65,16 +69,68 @@ public class SettingFragment extends Fragment {
                              Bundle savedInstanceState) {
 
         View view = inflater.inflate(R.layout.fragment_setting, container, false);
-        Button logoutBtn = view.findViewById(R.id.loggoutBtn);
+        Button changeBtn = view.findViewById(R.id.changeBtn); // 修改按钮
+        Button logoutBtn = view.findViewById(R.id.logoutBtn); // 登出按钮
         SharedPreferences sp = getActivity().getSharedPreferences("userdata", getActivity().MODE_PRIVATE);
-        SharedPreferences.Editor editor = sp.edit();
-        String userEmail = sp.getString("email", "null");
+        SharedPreferences.Editor editor = sp.edit(); // 创建sp存储的'修改器'
+        String userEmail = sp.getString("email", "null"); // 拿取SP里面的用户数据
 
+        // 获取用户输入'要改变的' email和password (输入控件)
+        username = view.findViewById(R.id.changeUsernameInput);
+        password = view.findViewById(R.id.changePasswordInput);
+
+        changeBtn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // 获取用户输入的新email和password
+                String newUsername = username.getText().toString().trim();
+                String newPassword = password.getText().toString().trim();
+
+                if(newUsername.isEmpty() && newPassword.isEmpty()) {
+                    // 用户俩都没填，直接点button (NO)
+                    Toast.makeText(getActivity(), "Please fill in one of the change field!", Toast.LENGTH_SHORT).show();
+                    Log.d("ChangeField", "用户俩空都没填就点了button :(");
+                    return;
+                }else{
+                    if(newUsername.isEmpty() && !newPassword.isEmpty()) {
+                        // 用户只填了password，没填username
+                        Toast.makeText(getActivity(), "Changed password: "+newPassword, Toast.LENGTH_SHORT).show();
+                        Log.d("PasswordChange", "修改的密码为: "+newPassword);
+                        //服务器操作
+
+                    }else if(!newUsername.isEmpty() && newPassword.isEmpty()) {
+                        // 用户只填了username
+                        Toast.makeText(getActivity(), "Changed username: "+newUsername, Toast.LENGTH_SHORT).show();
+                        Log.d("UsernameChange", "修改的用户名为: "+newUsername);
+                        //服务器操作
+
+                    }else{
+                        // 用户俩都填了
+                        Toast.makeText(getActivity(), "Changed username: "+username + " and password: "+password, Toast.LENGTH_SHORT).show();
+
+                        // 服务器操作
+
+
+                        }
+                    }
+
+                // 服务器操作成功后，修改本地SP数据 (否则在if-else if 跳出，并提示用户'修改失败')
+//                editor.putString("username", newUsername);
+//                editor.putString("password", newPassword);
+//                editor.apply();
+//                Log.d("SharedPreferences", "用户数据已修改");
+//                Toast.makeText(getActivity(), "用户数据已修改", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+
+        // 登出按钮的监听
         logoutBtn.setOnClickListener(new View.OnClickListener() {
              @Override
              public void onClick(View v) {
                  Toast.makeText(getActivity(),userEmail + " 现已登出", Toast.LENGTH_SHORT).show();
                  // 用户登出，清除SharedPreferences里面的用户登录信息
+                 editor.clear(); // 清除'当前用户'的 所有数据
                  editor.putBoolean("loginStatus", false);
                  editor.apply();
                  Log.d("SharedPreferences", "用户数据已登出");
